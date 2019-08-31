9f8757bb72600c680e843b0fbe0eec65fefb0238
slavic-sky <zirchik1997@mail.ru>
2019-09-01 00:16:16 +0300

    + main activity layout

--- app/src/main/java/ru/slavicsky/electroluxapp/MainActivity.java
+++ app/src/main/java/ru/slavicsky/electroluxapp/MainActivity.java
@@ -2,6 +2,7 @@ package ru.slavicsky.electroluxapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView view = findViewById(R.id.search_view);
     }
 }
