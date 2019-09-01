23dccf84fc26fa443d44f93843f80bba8f787e6b
slavic-sky <zirchik1997@mail.ru>
2019-09-01 14:07:33 +0300

    + transition button

--- app/src/main/java/ru/slavicsky/electroluxapp/MainActivity.java
+++ app/src/main/java/ru/slavicsky/electroluxapp/MainActivity.java
@@ -1,8 +1,13 @@
 package ru.slavicsky.electroluxapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView view = findViewById(R.id.search_view);
+        EditText search = findViewById(R.id.search_view);
+        Button getResultsButton = findViewById(R.id.results_button);
+
+        getResultsButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, ResultsActivity.class);
+            startActivity(intent);
+        });
     }
+
+
+
 }
---
+++ app/src/main/java/ru/slavicsky/electroluxapp/ResultsActivity.java
@@ -0,0 +1,12 @@
+package ru.slavicsky.electroluxapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultsActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+}
